a898ff51d13ef97e71cb4484890bc7a4e6f61c65
Jamey Gibson <jgibson@pindrop.com>
2018-11-08 10:59:04 -0500

    increase debounce timer to 8

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -45,7 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBUG_BOOT_TRACING
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCING_DELAY 8
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
