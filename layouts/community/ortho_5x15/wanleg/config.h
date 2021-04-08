b0bb5a3ac5b79d0df8f53027e1950df684e97759
Jamey Gibson <jcgtwo@gmail.com>
2021-04-07 23:47:07 -0400

    add jcgtwo preonic map, pull from upstream

--- layouts/community/ortho_5x15/wanleg/config.h
+++ layouts/community/ortho_5x15/wanleg/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //5x5 powered by Adafruit Feather 32u4 Bluefruit LE
-#if defined(KEYBOARD_5x5) && defined(BLUEFRUIT)
+#if defined(KEYBOARD_40percentclub_5x5) && defined(BLUEFRUIT)
 
 //need to undefine standard 5x5 array before defining alternate Bluefruit array
 #undef MATRIX_ROW_PINS
@@ -10,7 +10,4 @@
 //last 5 elements not tested
 #define MATRIX_COL_PINS { C6, D7, B5, B6, B7, D6, C7, F0, F1, F4, B1, B2, B3, D2, D3 }
 
-//get rid of description - too long to show properly in Windows
-#undef DESCRIPTION
-#define DESCRIPTION 
 #endif
